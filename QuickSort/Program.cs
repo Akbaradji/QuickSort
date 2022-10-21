88ec9363d2360f5122b290315092bd20bbaee3de
akbar <akbar.adji.ft21@mail.umy.ac.id>
2022-10-21 18:25:04 +0700

    Add Function Main

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -109,5 +109,16 @@ namespace QuickSort
         {
             return (n);
         }
+
+        static void Main(string[] args)
+        {
+            Program myList = new Program();
+            myList.input();
+            myList.q_sort(0, myList.getSize() -1);
+            myList.display();
+
+            Console.WriteLine("\n\nPress Enter to exit.");
+            Console.Read();
+        }
     }
 }
